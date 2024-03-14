962de69ef049557276509d19cab9428995419d2e
Sergiy <sergey.morenets@gmail.com>
2024-03-14 11:49:22 +0200

    Task 12.1 Migrate to Spring Boot Testcontainers

--- redis-task6/src/test/java/it/discovery/redis/BaseSpringDataRedisTest.java
+++ redis-task6/src/test/java/it/discovery/redis/BaseSpringDataRedisTest.java
@@ -1,8 +1,7 @@
 package it.discovery.redis;
 
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -11,10 +10,11 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public class BaseSpringDataRedisTest {
     @Container
+    @ServiceConnection
     static GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine").withExposedPorts(6379);
 
-    @DynamicPropertySource
-    static void mongoProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
-    }
+//    @DynamicPropertySource
+//    static void mongoProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+//    }
 }
